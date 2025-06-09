cd08ed337a33f0dbfa5b9505419ae633871ff703
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2025-06-09 18:08:11 +0530

    Changes

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { fileURLToPath } from 'url';
-import { dirname, resolve } from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      'pdfjs-dist/build/pdf.worker.js': resolve(
-        __dirname,
-        'node_modules/pdfjs-dist/build/pdf.worker.min.js'
-      ),
-    },
+  optimizeDeps: {
+    include: ['pdfjs-dist/build/pdf'],
+    exclude: ['pdfjs-dist/build/pdf.worker.mjs']
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          pdfjs: ['pdfjs-dist/build/pdf'],
+          'pdfjs-worker': ['pdfjs-dist/build/pdf.worker.mjs']
+        }
+      }
+    }
+  }
 });
